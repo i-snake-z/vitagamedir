e35e3ad1ba71ae57d960b9c58a4c61b133cee9ea
TheLastBilly <jtmonegro@gmail.com>
2020-09-16 21:16:35 -0400

    cleanned part of the code, moved to csv instead of xlsx, added TODO

--- vitagamedir.py
+++ vitagamedir.py
@@ -2,128 +2,96 @@
 #sys.path.append(r'\vita_app\venv\Lib\site-packages')
 
 import os
-from xlrd import open_workbook
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 import csv
 
+#Settings
+WINDOW_NAME     = "vitagamedir"
+WINDOW_HEIGHT   = 700
+WINDOW_WIDTH    = 900
 
-list1 =[]
-
-HEIGHT = 700
-WIDTH = 900
-
+#Tk window setup
 root = tk.Tk()
-
-root.title("Ps vita Directory games")
-
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+root.title(WINDOW_NAME)
+canvas = tk.Canvas(root, height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
 canvas.pack()
-
 background_image = tk.PhotoImage(file='background.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-
-
 # frame = tk.Frame(root, bg='#80c1ff', bd=5)
 # frame.place(relx=0.05, rely=0.02, relwidth=0.9, relheight=0.05)
-
-
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.05, rely=0.02, relwidth=0.9, relheight=0.1)
 
+#Buttons setup
+game_directory_entry = tk.Entry(frame, text= "Path to games")
+game_directory_entry.place(relx= 0.01, rely=0.01, relwidth=0.55, relheight=0.45)
+extract_directory_entry = tk.Entry(frame, text= "Directory to extract")
+extract_directory_entry.place(relx= 0.01, rely=0.5, relwidth=0.55, relheight=0.45)
 
+#Globals
+current_dir_games = []
 
-entry0 = tk.Entry(frame, text= "Directory of games")
-entry0.place(relx= 0.01, rely=0.01, relwidth=0.55, relheight=0.45)
+class db_entry:
+    code = ""
+    name = ""
 
-entry1 = tk.Entry(frame, text= "Directory to extract")
-entry1.place(relx= 0.01, rely=0.5, relwidth=0.55, relheight=0.45)
+    def __init__(self, code, name):
+        self.code = code 
+        self.name = name
 
+    def __eq__(self, other):
+        return self.code == other
+db_cache = []
 
-
-
+with open("./db.csv", "r") as db_file:
+    print("reading db.csv...")
+    csv_reader = csv.reader(db_file, delimiter=",")
+    for row in csv_reader:
+        entry = db_entry(row[0], row[1])
+        db_cache.append(entry)
+    print("done, found {} entries".format(len(db_cache)))
 
 def browseFiles():
     filename = filedialog.askdirectory()
-    entry0.delete(0, 'end')
-    entry0.insert(0, filename)
+    game_directory_entry.delete(0, 'end')
+    game_directory_entry.insert(0, filename)
 
 def browseFilesextract():
     filename = filedialog.askdirectory()
-    entry1.delete(0, 'end')
-    entry1.insert(0, filename)
+    extract_directory_entry.delete(0, 'end')
+    extract_directory_entry.insert(0, filename)
 
 def clear():
-
-    entry0.delete(0, 'end')
-    entry1.delete(0, 'end')
-
-
+    game_directory_entry.delete(0, 'end')
+    extract_directory_entry.delete(0, 'end')
 
 def names():
-
-    list1.clear()
+    current_dir_games.clear()
 
     lower_frame = tk.Frame(root, bd=5, bg='#b3e7fd')
     lower_frame.place(relx=0.1, rely=0.15, relwidth=0.80, relheight=0.80)
-
     label = tk.Label(lower_frame)
     label.place(relwidth=1, relheight=1)
 
+    path = game_directory_entry.get()
+    directory_list = os.listdir(path)
 
+    for directory in directory_list:
+        for entry in db_cache:
+            if entry == directory:
+                print("Found {}".format(directory))
+                current_dir_games.append([entry.code,entry.name])
 
-
-    path = entry0.get()
-    nameg = os.listdir(path)
-
-    book = open_workbook('ultimate.xlsx')
-
-    for x in nameg:
-        for sheet in book.sheets():
-            for rowidx in range(sheet.nrows):
-                row = sheet.row(rowidx)
-                for colidx, cell in enumerate(row):
-                    if cell.value == x :
-                        first_sheet = book.sheet_by_index(0)
-                        cell = first_sheet.cell(rowidx, colidx)
-                        cell1 = first_sheet.cell(rowidx,colidx + 1)
-
-                        cell2 = str(cell)
-                        cell3 = str(cell1)
-
-
-                        cells = cell2.replace("text:","")
-                        cells2 = cell3.replace("text:", "")
-
-                        cellf1 = cells.replace("'", "")
-                        cellf2 = cells2.replace("'", "")
-
-                        list1.append([cellf1, cellf2])
-
-
-
-
-
-
-
-
-    tempList = list1
+    tempList = current_dir_games
     cols = ('ID_GAME', 'NAME OF GAME')
     listBox = ttk.Treeview(lower_frame, columns=cols, show='headings')
 
-
-
-
     for i, (name, score) in enumerate(tempList, start=1):
         listBox.insert("", "end", values=(name, score))
-
-
-
-
-
 
     # set column headings
     for col in cols:
@@ -134,12 +102,8 @@ def names():
 
 
 def extract():
-    pathextract = entry1.get()
-
- 
-
+    pathextract = extract_directory_entry.get()
     fields = ['ID', 'GAME_ID']
-
 
     filename =  pathextract + "/export.csv"
     dirname = os.path.dirname(filename)
@@ -152,12 +116,7 @@ def extract():
         write = csv.writer(f, delimiter=';')
 
         write.writerow(fields)
-        write.writerows(list1)
-
-
-
-
-
+        write.writerows(current_dir_games)
 
 bt = tk.Button(frame, text = "Chose Directory of Games" ,command= browseFiles)
 bt.place(relx= 0.57, rely=0.01, relwidth=0.30, relheight=0.45)
@@ -173,8 +132,5 @@ bt3.place(relx=0.88, rely=0.5, relwidth=0.05, relheight=0.45)
 
 bt4 = tk.Button(frame, text = "CLEAR" ,command= clear)
 bt4.place(relx=0.94, rely=0.01, relwidth=0.06, relheight=0.95)
-
-
-
 
 root.mainloop()
