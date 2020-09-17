0acf74c3d7da2be9f678072e1f2a1900fec3f9c5
i-snake-z <71404370+i-snake-z@users.noreply.github.com>
2020-09-17 21:05:14 +0100

    Add some estability
    Add:
    Pop up messages for errors and for extraction
    clear button now clear the name of the games too

--- vitagamedir.py
+++ vitagamedir.py
@@ -4,6 +4,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 from tkinter import ttk
 import csv
 
@@ -21,8 +22,7 @@ background_image = tk.PhotoImage(file='background.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-# frame = tk.Frame(root, bg='#80c1ff', bd=5)
-# frame.place(relx=0.05, rely=0.02, relwidth=0.9, relheight=0.05)
+
 frame = tk.Frame(root, bg='#80c1ff', bd=5)
 frame.place(relx=0.05, rely=0.02, relwidth=0.9, relheight=0.1)
 
@@ -40,20 +40,24 @@ class db_entry:
     name = ""
 
     def __init__(self, code, name):
-        self.code = code 
+        self.code = code
         self.name = name
 
     def __eq__(self, other):
         return self.code == other
+
 db_cache = []
 
 with open("./db.csv", "r", encoding="mbcs") as db_file:
-    print("reading db.csv...")
+    #print("reading db.csv...")
     csv_reader = csv.reader(db_file, delimiter=",")
     for row in csv_reader:
-        entry = db_entry(row[0], row[1])
-        db_cache.append(entry)
-    print("done, found {} entries".format(len(db_cache)))
+        gameinfile = db_entry(row[0], row[1])
+        db_cache.append(gameinfile)
+    #print("done, found {} entries".format(len(db_cache)))
+
+
+
 
 def browseFiles():
     filename = filedialog.askdirectory()
@@ -65,72 +69,97 @@ def browseFilesextract():
     extract_directory_entry.delete(0, 'end')
     extract_directory_entry.insert(0, filename)
 
-def clear():
-    game_directory_entry.delete(0, 'end')
-    extract_directory_entry.delete(0, 'end')
+
+
 
 def names():
     current_dir_games.clear()
-
-    lower_frame = tk.Frame(root, bd=5, bg='#b3e7fd')
-    lower_frame.place(relx=0.1, rely=0.15, relwidth=0.80, relheight=0.80)
-    label = tk.Label(lower_frame)
-    label.place(relwidth=1, relheight=1)
-
     path = game_directory_entry.get()
-    directory_list = os.listdir(path)
 
-    for directory in directory_list:
-        for entry in db_cache:
-            if entry == directory:
-                print("Found {}".format(directory))
-                current_dir_games.append([entry.code,entry.name])
+    if path == "":
+        messagebox.showinfo("Info", "Please insert path of the games")
 
-    tempList = current_dir_games
-    cols = ('ID_GAME', 'NAME OF GAME')
-    listBox = ttk.Treeview(lower_frame, columns=cols, show='headings')
+    else:
+        lower_frame = tk.Frame(root, bd=5, bg='#b3e7fd')
+        lower_frame.place(relx=0.1, rely=0.15, relwidth=0.80, relheight=0.80)
 
-    for i, (name, score) in enumerate(tempList, start=1):
-        listBox.insert("", "end", values=(name, score))
+        def clear():
+            game_directory_entry.delete(0, 'end')
+            extract_directory_entry.delete(0, 'end')
+            lower_frame.destroy()
 
-    # set column headings
-    for col in cols:
-        listBox.heading(col, text=col)
-    listBox.grid(row=1, column=0, columnspan=2)
+        bt_clear = tk.Button(frame, text="CLEAR", command=clear)
+        bt_clear.place(relx=0.94, rely=0.01, relwidth=0.06, relheight=0.95)
 
-    listBox.pack(fill='both',expand=1)
+        # path = game_directory_entry.get()
+        directory_list = os.listdir(path)
+
+        for directory in directory_list:
+            for game in db_cache:
+                if game == directory:
+                    # print("Found {}".format(directory))
+                    current_dir_games.append([game.code, game.name])
+
+        tempList = current_dir_games
+        cols = ('ID_GAME', 'NAME OF GAME')
+        listBox = ttk.Treeview(lower_frame, columns=cols, show='headings')
+
+        for i, (name, score) in enumerate(tempList, start=1):
+            listBox.insert("", "end", values=(name, score))
+
+        # set column headings
+        for col in cols:
+            listBox.heading(col, text=col)
+        listBox.grid(row=1, column=0, columnspan=2)
+
+        listBox.pack(fill='both', expand=1)
 
 
 def extract():
     pathextract = extract_directory_entry.get()
-    fields = ['ID', 'GAME_ID']
 
-    filename =  pathextract + "/export.csv"
-    dirname = os.path.dirname(filename)
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
+    if pathextract == "":
+        messagebox.showinfo("Info", "Please insert path to extract")
 
-    with open(filename, 'w', newline='') as f:
+    else:
+        fields = ['ID', 'GAME_ID']
 
-        # using csv.writer method from CSV package
-        write = csv.writer(f, delimiter=';')
+        filename = pathextract + "/export.csv"
+        dirname = os.path.dirname(filename)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
 
-        write.writerow(fields)
-        write.writerows(current_dir_games)
+        with open(filename, 'w', newline='') as f:
 
-bt = tk.Button(frame, text = "Chose Directory of Games" ,command= browseFiles)
-bt.place(relx= 0.57, rely=0.01, relwidth=0.30, relheight=0.45)
+            # using csv.writer method from CSV package
+            write = csv.writer(f, delimiter=';')
 
-bt1 = tk.Button(frame, text = "GO" ,command= names)
-bt1.place(relx=0.88, rely=0.01, relwidth=0.05, relheight=0.45)
+            write.writerow(fields)
+            write.writerows(current_dir_games)
 
-bt2 = tk.Button(frame, text = "Chose Directory to Extract CSV" ,command= browseFilesextract)
-bt2.place(relx= 0.57, rely=0.5, relwidth=0.30, relheight=0.45)
+        messagebox.showinfo("Info", "Extract Complete")
 
-bt3 = tk.Button(frame, text = "GO" ,command= extract)
-bt3.place(relx=0.88, rely=0.5, relwidth=0.05, relheight=0.45)
+def snake_():
+    messagebox.showinfo("Enjoy", "Hope you like this app :)")
 
-bt4 = tk.Button(frame, text = "CLEAR" ,command= clear)
-bt4.place(relx=0.94, rely=0.01, relwidth=0.06, relheight=0.95)
+
+
+bt_browse_game_folder = tk.Button(frame, text = "Chose Directory of Games" ,command= browseFiles)
+bt_browse_game_folder.place(relx= 0.57, rely=0.01, relwidth=0.30, relheight=0.45)
+
+bt_go_dir_games = tk.Button(frame, text = "GO" ,command= names)
+bt_go_dir_games.place(relx=0.88, rely=0.01, relwidth=0.05, relheight=0.45)
+
+bt_browse_extract_folder = tk.Button(frame, text = "Chose Directory to Extract CSV" ,command= browseFilesextract)
+bt_browse_extract_folder.place(relx= 0.57, rely=0.5, relwidth=0.30, relheight=0.45)
+
+bt_go_dir_extract = tk.Button(frame, text = "GO" ,command= extract)
+bt_go_dir_extract.place(relx=0.88, rely=0.5, relwidth=0.05, relheight=0.45)
+
+
+snake = tk.Button(root, text = "i-snake-z",command= snake_)
+snake.place(relx=0.93, rely=0.955, relwidth=0.06, relheight=0.03)
+
+
 
 root.mainloop()
